c898f31b1113ae8dc1983c6d6eafce19f2c372b6
Sudo-Self <sudo-self@proton.me>
2023-12-20 23:42:39 -0700

    Add files via upload

---
+++ unocss.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig, presetAttributify, presetIcons, presetUno, transformerDirectives, transformerVariantGroup } from 'unocss'
+
+export default defineConfig({
+  shortcuts: {
+    'border-base': 'border-gray-200 dark:border-dark-100',
+    'border-dark-only': 'border-transparent dark:border-dark-100',
+    'bg-base': 'bg-white dark:bg-[#181818]',
+    'color-base': 'text-gray-900 dark:text-gray-300',
+    'color-fade': 'text-gray-900:50 dark:text-gray-300:50',
+    'icon-button': 'op50 hover:op100 my-auto',
+  },
+  presets: [
+    presetUno(),
+    presetIcons(),
+    presetAttributify(),
+  ],
+  transformers: [
+    transformerVariantGroup(),
+    transformerDirectives(),
+  ],
+  theme: {
+    colors: {
+      primary: 'var(--theme-color)',
+      dark: {
+        100: '#222',
+        200: '#333',
+        300: '#444',
+        400: '#555',
+        500: '#666',
+        600: '#777',
+        700: '#888',
+        800: '#999',
+        900: '#aaa',
+      },
+    },
+  },
+})
